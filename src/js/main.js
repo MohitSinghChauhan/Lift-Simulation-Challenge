420f526f1f65fec8110d99a66f4f401a1592d583
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-07-06 21:57:58 +0530

    feat: Implement Generation of Responsive lift Simulation UI

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,64 @@ submitBtn.addEventListener('click', () => {
     liftSimulationContainer.classList.remove('hide');
     inputContainer.classList.add('hide');
   }
+
+  buildLiftSimulationUI(floors, lifts);
+
 });
+
+const buildLiftSimulationUI = (floors, lifts) => {
+  const container = document.getElementsByClassName('floors-container')[0];
+  let liftContainerWidth = null;
+
+  for (let i = 0; i < floors; i++) {
+    const floor = document.createElement('div');
+    floor.className = 'floor';
+
+    const floorButtonContainer = document.createElement('div');
+    floorButtonContainer.className = 'floor-button-container';
+
+    const upButton = document.createElement('button');
+    upButton.className = 'floor-button';
+    upButton.innerText = '🔼';
+
+    const downButton = document.createElement('button');
+    downButton.className = 'floor-button';
+    downButton.innerText = '🔽';
+
+    floorButtonContainer.appendChild(upButton);
+    floorButtonContainer.appendChild(downButton);
+
+    const liftContainer = document.createElement('div');
+    liftContainer.className = 'lift-container';
+
+    if (i === 0) {
+      for (let j = 0; j < lifts; j++) {
+        const lift = document.createElement('div');
+        lift.className = `lift lift-${j + 1}` ;
+        liftContainer.appendChild(lift);
+      }
+      console.log(liftContainerWidth);
+    }
+
+    // Setting the width of the grounf floor lift container to the other floor lift containers
+    if (i !== 0) {
+      liftContainer.style.width = liftContainerWidth + 'px';
+      console.log(liftContainerWidth);
+    }
+    // liftContainer.style.width = liftContainerWidth + 'px';
+
+    const floorNumber = document.createElement('div');
+    floorNumber.className = 'floor-number';
+    floorNumber.innerText = i === 0 ? 'G' : i;
+
+    floor.appendChild(floorButtonContainer);
+    floor.appendChild(liftContainer);
+    floor.appendChild(floorNumber);
+
+    container.appendChild(floor);
+    if (i === 0) {
+      liftContainerWidth = liftContainer.offsetWidth;
+    }
+    console.log(liftContainerWidth);
+  }
+};
