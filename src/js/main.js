fc222229a8a6d78442877d1188faff200300b43d
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-07-09 02:50:11 +0530

    feat: lift simulation logic implemented

--- src/js/main.js
+++ src/js/main.js
@@ -4,25 +4,26 @@ const liftSimulationContainer = document.getElementsByClassName(
   'lift-simulation-container'
 )[0];
 
-let noOfFloors = 0;
-let noOfLifts = 0;
+// Initialized lift
+let noOfFloors = 8;
+let noOfLifts = 6;
 
-submitBtn.addEventListener('click', () => {
-  const noOfFloorsEl = document.getElementById('floors');
-  const noOfLiftsEl = document.getElementById('lifts');
-  noOfFloors = noOfFloorsEl.value;
-  noOfLifts = noOfLiftsEl.value;
+// submitBtn.addEventListener('click', () => {
+//   const noOfFloorsEl = document.getElementById('floors');
+//   const noOfLiftsEl = document.getElementById('lifts');
+//   noOfFloors = noOfFloorsEl.value;
+//   noOfLifts = noOfLiftsEl.value;
 
-  if (noOfFloors === '' || noOfFloors === 0 || noOfLifts === '') {
-    alert('Please enter the number of floors and lifts!');
-  } else {
-    liftSimulationContainer.classList.remove('hide');
-    inputContainer.classList.add('hide');
-  }
+//   if (noOfFloors === '' || noOfFloors === 0 || noOfLifts === '') {
+//     alert('Please enter the number of floors and lifts!');
+//   } else {
+liftSimulationContainer.classList.remove('hide');
+inputContainer.classList.add('hide');
+//   }
 
-  buildLiftSimulationUI(noOfFloors, noOfLifts);
-  simulateLifts();
-});
+//   buildLiftSimulationUI(noOfFloors, noOfLifts);
+//   simulateLifts();
+// });
 
 const buildLiftSimulationUI = (floors, lifts) => {
   const container = document.getElementsByClassName('floors-container')[0];
@@ -39,6 +40,7 @@ const buildLiftSimulationUI = (floors, lifts) => {
       const upButton = document.createElement('button');
       upButton.className = 'floor-button';
       upButton.innerText = '🔼';
+      upButton.setAttribute('data-floor', (i + 1).toString());
       floorButtonContainer.appendChild(upButton);
     }
 
@@ -46,6 +48,7 @@ const buildLiftSimulationUI = (floors, lifts) => {
       const downButton = document.createElement('button');
       downButton.className = 'floor-button';
       downButton.innerText = '🔽';
+      downButton.setAttribute('data-floor', (i + 1).toString());
       floorButtonContainer.appendChild(downButton);
     }
 
@@ -77,27 +80,33 @@ const buildLiftSimulationUI = (floors, lifts) => {
     if (i === 0) {
       liftContainerWidth = liftContainer.offsetWidth;
     }
-    console.log(liftContainerWidth);
   }
 };
 
-
 // Logic for the simulating lifts
-
 const simulateLifts = () => {
+  // Add event listeners to all floor buttons
+  const floorButtons = document.querySelectorAll('.floor-button');
+  floorButtons.forEach((button) => {
+    button.addEventListener('click', handleFloorButtonClick);
+  });
+
   class Lift {
     constructor() {
-      this.currentFloor = 0;
-      this.direction = 'up';
+      this.domEl = null;
+      this.currentFloor = 1;
       this.isMoving = false;
+      this.targetFloor = 0;
+      this.floorHeight = 120; // height of 1 floor in px
     }
   }
 
-  // Creating instances of Lift given by user
-  function createLifts(n) {
+  // Create instances of Lift given by user
+  function createLifts(numLifts) {
     const lifts = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < numLifts; i++) {
       const lift = new Lift();
+      lift.domEl = document.querySelector(`.lift-${i + 1}`);
       lifts.push(lift);
     }
     return lifts;
@@ -105,4 +114,69 @@ const simulateLifts = () => {
 
   const lifts = createLifts(noOfLifts);
   console.log(lifts);
+
+  // Handle floor button click event
+  function handleFloorButtonClick(event){
+    const targetFloor = parseInt(event.target.dataset.floor);
+    requestLift(targetFloor); // Call the function to handle the requested floor
+  };
+
+  const requestLift = (requestedFloor) => {
+    const selectedLift = selectLift(requestedFloor);
+    selectedLift.targetFloor = requestedFloor;
+    moveLift(selectedLift);
+  };
+
+  const moveLift = (selectedLift) => {
+    const floorsInBetween = selectedLift.targetFloor - selectedLift.currentFloor;
+    const distance = Math.abs(floorsInBetween) * selectedLift.floorHeight;
+    const transitionTime = Math.abs(floorsInBetween) * 2000;
+
+    selectedLift.isMoving = true;
+
+    const liftElement = selectedLift.domEl;
+    const computedTransform = liftElement.style.transform;
+    const translateYValue = computedTransform ? Math.abs(parseInt(computedTransform.match(/-?\d+/)[0])) : 0;
+
+    liftElement.style.transition = `transform ${transitionTime}ms ease-in-out`;
+    if (floorsInBetween > 0)
+      liftElement.style.transform = `translateY(-${distance + translateYValue}px)`;
+    else
+      liftElement.style.transform = `translateY(-${Math.abs(distance - translateYValue)}px)`;
+
+    setTimeout(() => {
+      selectedLift.isMoving = false;
+      selectedLift.currentFloor = selectedLift.targetFloor;
+    }, transitionTime);
+  };
+
+  const selectLift = (floor) => {
+    let selectedLift = null;
+    let minDistance = Infinity;
+
+    for (const lift of lifts) {
+      const distance = Math.abs(lift.currentFloor - floor);
+      if (distance < minDistance && !lift.isMoving) {
+        minDistance = distance;
+        selectedLift = lift;
+      }
+    }
+
+    // If no stationary lifts are available, find the nearest lift
+    if (!selectedLift) {
+      for (const lift of lifts) {
+        const distance = Math.abs(lift.currentFloor - floor);
+        if (distance < minDistance) {
+          minDistance = distance;
+          selectedLift = lift;
+        }
+      }
+    }
+
+    return selectedLift;
+  };
 };
+
+// Temporary changes to skip every time inputs of lift and floors
+buildLiftSimulationUI(noOfFloors, noOfLifts);
+simulateLifts();
